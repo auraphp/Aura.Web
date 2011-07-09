bb22ba0a127b2e7ce2e399b628610d6e6ae17301
Hari K T <kthari85@gmail.com>
2011-07-09 08:16:15 +0530

    adding licence to missed files

--- scripts/instance.php
+++ scripts/instance.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * 
+ * This file is part of the Aura project for PHP.
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ */
 namespace Aura\Web;
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src.php';
 
