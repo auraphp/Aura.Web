9e198ceba22be1b768bd0da371727fc9a8e6c9af
Paul M. Jones <pmjones88@gmail.com>
2011-10-03 10:41:33 -0500

    config now adds to autoloader

--- config/default.php
+++ config/default.php
@@ -1,11 +1,9 @@
 <?php
 /**
- * 
- * This file is part of the Aura project for PHP.
- * 
- * @license http://opensource.org/licenses/bsd-license.php BSD
- * 
+ * Package prefix for autoloader.
  */
+$loader->addPrefix('Aura\Web\\', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src');
+
 /**
  * Constructor params.
  */
