bc4adac05bcab7f78dac85f41232ef86d815f706
Paul M. Jones <pmjones88@gmail.com>
2013-06-15 18:10:40 -0600

    continue testing

--- Aura.Web/src/Request/Superglobal.php
+++ Aura.Web/src/Request/Superglobal.php
@@ -10,11 +10,16 @@ class Superglobal
         $this->data = $data;
     }
     
-    public function get($key, $alt = null)
+    public function get($key = null, $alt = null)
     {
+        if (! $key) {
+            return $this->data;
+        }
+        
         if (array_key_exists($key, $this->data)) {
             return $this->data[$key];
         }
+        
         return $alt;
     }
 }
